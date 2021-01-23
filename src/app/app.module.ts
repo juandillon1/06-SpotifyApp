d12d346517208e2c4e6160a240c4da21fb72659f
Juancito <juancruzdillon123@gmail.com>
2021-01-23 13:14:08 -0300

    cambios

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+//Importar rutas
+import { ROUTES } from './app.routes';
+
+//Importar Componentes
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+
+//pipes
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { NoimagePipe } from './pipes/noimage.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent,
+    NoimagePipe,
+    TarjetasComponent,
+    LoadingComponent,
+    DomseguroPipe
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES, { useHash: true })
+  ],
+  providers: [DatePipe],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
